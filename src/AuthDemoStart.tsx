f0367faef7f8d5e5a6dfd52f1b784353c2005fba
christian-2009 <c-sophocleous@hotmail.com>
2022-06-23 11:46:22 +0100

    got sign in and sign out working on front end

--- src/AuthDemoStart.tsx
+++ src/AuthDemoStart.tsx
@@ -1,8 +1,12 @@
 import axios from "axios";
 import React, { useState } from "react";
+import {auth, googleAuthProvider} from './configureFirebase'
+import { signInWithPopup, UserCredential } from "firebase/auth";
 
 export function AuthDemoStart(): JSX.Element {
+
     const [lastAPIReply, setLastAPIReply] = useState<string>("");
+    const [user, setUser] = useState<UserCredential | null>()
 
 
     async function handleFetchTimeClicked() {
@@ -16,12 +20,25 @@ export function AuthDemoStart(): JSX.Element {
         setLastAPIReply(reply.data);
     }
 
+    const  handleSignIn = async () => {
+       const response = await signInWithPopup(auth, googleAuthProvider)
+       setUser(response)
+       console.log(user)
+    }
+
+    const handleSignOut = async () => {
+        auth.signOut()
+        setUser(null)
+    }
+
     return (
         <div>
             <h2>Auth Demo</h2>
+            {user &&<h3>Hello {user?.user.displayName}</h3>}
+            {user?.user.photoURL && <img src={user?.user.photoURL} alt='mug shot'/>}
 
-            <button onClick={() => alert("not implemented")}>Sign in</button>
-            <button onClick={() => alert("not implemented")}>Sign out</button>
+            <button onClick={handleSignIn}>Sign in</button>
+            <button onClick={handleSignOut}>Sign out</button>
 
             <hr />
             <h3>Talk to the API</h3>
@@ -30,6 +47,7 @@ export function AuthDemoStart(): JSX.Element {
             <h4>Last successful reply from API</h4>
             <div>{lastAPIReply}</div>
             <br />
+            <pre>{JSON.stringify(user, null, 2)}</pre>
             <i>(also check console for any failures)</i>
 
             <hr />
